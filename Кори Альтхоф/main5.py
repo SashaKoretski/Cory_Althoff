25c935864b0e391cdfc4ea1e8cec25ddf95aa007
SashaKoretski <59177189+SashaKoretski@users.noreply.github.com>
2024-03-16 20:58:48 +0300

    Удалил лишнюю папку

--- Кори Альтхоф/main5.py
+++
@@ -1,3 +0,0 @@
-list1 = ["selftaught", "code", "sit", "eat", "programming", "dinner", "one", "two", "coding", "a", "tech"]
-list2 = [c for c in list1 if len(c) > 4]
-print(list2)
--- Кори Альтхоф/main9.py
+++
@@ -1,7 +0,0 @@
-n = int(input("n = "))
-an_array = range(1, n)
-odd_array = [c for c in an_array if c % 2 == 1]
-even_array = [c for c in an_array if c % 2 == 0]
-
-the_array = even_array + odd_array
-print(the_array)
